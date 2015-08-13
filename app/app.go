aa85f8e1dee0443e9860aa930d5d7bdf55544a6f
Doug Richardson <dougie.richardson@gmail.com>
2015-08-13 13:30:56 -0700

    serial subrequest example that respects context timeout

--- app/app.go
+++ app/app.go
@@ -1,9 +1,11 @@
 package app
 
 import (
+	"fmt"
 	clog "github.com/drichardson/go-appengine-vm-example/contextlog"
 	"github.com/drichardson/go-appengine-vm-example/handler"
 	"golang.org/x/net/context"
+	"io/ioutil"
 	"net/http"
 	"time"
 )
@@ -13,6 +15,8 @@ func RegisterHandlers() {
 	handler.Handle("/datastore/put", handleDatastorePut)
 	handler.Handle("/datastore/get", handleDatastoreGet)
 	handler.Handle("/slow/get", handleSlowGet)
+	handler.Handle("/subrequests/serial", handleSerialSubrequests)
+	handler.Handle("/subrequests/concurrent", handleConcurrentSubrequests)
 }
 
 func handlePing(c context.Context, w http.ResponseWriter, r *http.Request) {
@@ -44,23 +48,93 @@ func handleSlowGet(c context.Context, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	time.Sleep(delay)
-	w.WriteHeader(http.StatusNoContent)
+	w.Write([]byte(fmt.Sprintf("delayed %v\n", delayStr)))
 }
 
-func handleTimeout(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	/*
-		ctx, cancel := context.WithTimeout(ctx, time.Duration(1)*time.Second)
-		defer cancel()
+func localURL(path string) string {
+	return "http://localhost:8080/" + path
+}
 
-		c := make(chan error, 1)
-		go func() {
-			clog.Debug("Sleeping 5 seconds")
-			time.Sleep(time.Duration(5) * time.Second)
-			clog.Debug("Done sleeping")
-		}()
+func handleSerialSubrequests(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 
-		select {
-		case <-ctx.Done():
+	// Require the caller to specify an overall timeout, like 700ms.
+	timeoutStr := r.URL.Query().Get("timeout")
+	timeout, err := time.ParseDuration(timeoutStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Invalid timeout query parameter. Expected something like timeout=700ms\n"))
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	var sub1Result, sub2Result string
+
+	start := time.Now()
+
+	for i := 0; i < 2; i++ {
+		err = httpGet(ctx, localURL("slow/get?delay=500ms"), func(r *http.Response, err error) error {
+			if err != nil {
+				clog.Debugf(ctx, "handleTimeout: get %v failed.", i, err)
+				return err
+			}
+			b, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				clog.Debug(ctx, "handleTimeout: get %v ReadAll failed.", i, err)
+				return err
+			}
+			clog.Debug(ctx, "handleTimeout: get %v returned with:", i, string(b))
+			sub1Result = string(b)
+			return nil
+		})
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(fmt.Sprintf("Sub request %v failed. %v\n", i, err.Error())))
+			return
 		}
-	*/
+	}
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Second sub request failed. " + err.Error() + "\n"))
+		return
+	}
+
+	duration := time.Since(start)
+
+	result := fmt.Sprintf("Duration: %v\nFirst: %vSecond: %v", duration, sub1Result, sub2Result)
+	w.Write([]byte(result))
+}
+
+func handleConcurrentSubrequests(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+}
+
+func httpGet(ctx context.Context, url string, f func(*http.Response, error) error) error {
+	r, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	return httpDo(ctx, r, f)
+}
+
+// Taken from (Sameer Ajmani's blog post)[https://blog.golang.org/context].
+// Runs the HTTP request and processes its response in a new goroutine. It
+// cancels the request if ctx.Done is closed before the goroutine exits:
+func httpDo(ctx context.Context, req *http.Request, f func(*http.Response, error) error) error {
+	// Run the HTTP request in a goroutine and pass the response to f.
+	tr := &http.Transport{}
+	client := &http.Client{Transport: tr}
+	c := make(chan error, 1)
+	go func() { c <- f(client.Do(req)) }()
+	select {
+	case <-ctx.Done():
+		tr.CancelRequest(req)
+		<-c // Wait for f to return.
+		return ctx.Err()
+	case err := <-c:
+		return err
+	}
 }
--- contextlog/contextlog.go
+++ contextlog/contextlog.go
@@ -12,6 +12,14 @@ func Debug(c context.Context, args ...interface{}) {
 	log.Println("DEBUG: " + fmt.Sprint(args...))
 }
 
+func Debugf(c context.Context, format string, args ...interface{}) {
+	log.Printf("DEBUG: "+format, args...)
+}
+
 func Error(c context.Context, args ...interface{}) {
 	log.Println("ERROR: " + fmt.Sprint(args...))
+}
+
+func Errorf(c context.Context, format string, args ...interface{}) {
+	log.Printf("ERROR: "+format, args...)
 }
