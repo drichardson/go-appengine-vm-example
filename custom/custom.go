985fa61c3c22c4570e28cea8ad294f962188215a
Doug Richardson <dougie.richardson@gmail.com>
2015-08-14 09:11:36 -0700

    lifecycle handlers for custom runtimes

--- custom/custom.go
+++ custom/custom.go
@@ -1,16 +1,34 @@
-// This example only works on Managed VMs. Don't use goapp (the
-// classic environment for Go App Engine) to build.
+// An App Engine Managed VM Custom Runtime, as described here:
+// https://cloud.google.com/appengine/docs/managed-vms/custom-runtimes
 
 package main
 
 import (
 	"github.com/drichardson/go-appengine-vm-example/app"
+	clog "github.com/drichardson/go-appengine-vm-example/contextlog"
+	"github.com/drichardson/go-appengine-vm-example/handler"
+	"golang.org/x/net/context"
 	"log"
 	"net/http"
 )
 
 func main() {
 	app.RegisterHandlers()
+	registerLifecycleEventHandlers()
 	err := http.ListenAndServe(":8080", nil)
 	log.Fatal(err)
+}
+
+// App Engine Managed VMs Custom Runtimes must implement the App Engine
+// lifecycle handlers.
+// https://cloud.google.com/appengine/docs/managed-vms/custom-runtimes
+func registerLifecycleEventHandlers() {
+	handler.Handle("/_ah/start", okHandler)
+	handler.Handle("/_ah/stop", okHandler)
+	handler.Handle("/_ah/health", okHandler)
+}
+
+func okHandler(c context.Context, w http.ResponseWriter, r *http.Request) {
+	clog.Debugf(c, "Responding with 'ok' for %v", r.URL)
+	w.Write([]byte("ok"))
 }
